a07467c33953b5685c4420618e3b68982fc6b4ba
Mohamed Zaki <mohamedzzaki2000s@gmail.com>
2026-01-05 22:00:36 +0200

    fixing the grid layout for the jason gallery section

--- src/components/Sections/JasonGallery.jsx
+++ src/components/Sections/JasonGallery.jsx
@@ -1,27 +1,30 @@
-import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 gsap.registerPlugin(ScrollTrigger);
 
 function JasonGallery() {
-  useGSAP(() => {
-    gsap.set('.jason', { marginTop: '-80vh' });
+  // useGSAP(() => {
+  //   gsap.set('.jason', { marginTop: '-80vh' });
 
-    const tl = gsap.timeline();
-    tl.to('.first-vd', { opacity: 0, duration: 1, ease: 'power1.inOut' });
+  //   const tl = gsap.timeline();
+  //   tl.to('.first-vd', { opacity: 0, duration: 1, ease: 'power1.inOut' });
 
-    ScrollTrigger.create({
-      trigger: '.jason',
-      start: 'top 90%',
-      end: '10% center',
-      scrub: 2,
-      animation: tl,
-    });
-  });
+  //   ScrollTrigger.create({
+  //     trigger: '.jason',
+  //     start: 'top 90%',
+  //     end: '10% center',
+  //     scrub: 2,
+  //     animation: tl,
+  //   });
+  // });
   return (
-    <section className="jason" data-speed="1.1">
-      <div className="jason-content max-w-lg">
+    <section
+      className="jason auto-cols-fr grid-cols-1 gap-3 md:grid md:grid-cols-[0.7fr_1fr]"
+      data-speed="1.1"
+    >
+      {/* first */}
+      <div className="jason-content mb-15 w-full border pl-10">
         <h1>Jason Duval</h1>
         <h2 className="font-bold">
           Jason wants an easy life, but things just keep getting harder.
@@ -31,19 +34,27 @@ function JasonGallery() {
           his troubled teens, he found himself in the Keys doing what he knows best, working for
           local drug runners. It might be time to try something new.
         </p>
-
-        <div className="jason-2">
-          <img src="/images/jason-2.webp" />
-        </div>
       </div>
 
-      <div className="img-box order-0 space-y-5 lg:mt-80">
-        <div className="jason-1">
-          <img src="/images/jason-1.webp" alt="" />
-        </div>
-        <div className="jason-3">
-          <img src="/images/jason-3.webp" alt="" />
-        </div>
+      {/* second */}
+      <div className="md:mt-30">
+        <img
+          src="/images/jason-1.webp"
+          className="h-full w-full object-cover [object-position:5%_center]"
+        />
+      </div>
+
+      {/* third */}
+      <div className="md:-mt-8 md:h-[80vh] lg:-mt-27">
+        <img
+          src="/images/jason-2.webp"
+          className="h-full w-full object-cover [object-position:80%_center]"
+        />
+      </div>
+
+      {/* fourth */}
+      <div className="">
+        <img src="/images/jason-3.webp" alt="" />
       </div>
     </section>
   );
